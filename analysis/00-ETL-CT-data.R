349b9d327a3b2ef4d1b4993d47db600168527f40
Aditya Lahiri <lahiria@chop.edu>
2024-02-14 17:24:47 -0500

    initiat etl script for CT data

---
+++ analysis/00-ETL-CT-data.R
@@ -0,0 +1,33 @@
+# Load libraries
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(data.table)
+  library(ggplot2)
+  library(tidyverse)
+  library(stringi)
+  library(qdapRegex)
+  library(jsonlite)
+  library(httr)
+  library(biomaRt)
+  library(ghql)
+  library(readxl)
+  # For embedding
+  library(word2vec)
+  library(keras)
+  library(reticulate)
+  library(purrr)
+  library(stringdist)
+})
+
+
+# Set the directories
+setwd(getwd())
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+util_dir <- file.path(root_dir, "util")
+data_dir <- file.path(root_dir,"data")
+input_dir <- file.path(root_dir,"input")
+analysis_dir <- file.path(root_dir,"analysis")
+intermediate_dir <- file.path(analysis_dir,"intermediate")
+
+
+
