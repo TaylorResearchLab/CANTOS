1a3186ec840639643bf5c7776de43fe31d234eda
Aditya Lahiri <lahiria@chop.edu>
2024-02-15 18:33:01 -0500

    add script to generate ct tumor file for manual annotation and validation

---
+++ analysis/01-generate-disease-annotation-for-manual-review.R
@@ -0,0 +1,62 @@
+# Load libraries
+suppressPackageStartupMessages({
+  library(dplyr)
+})
+
+
+# Set the directories
+setwd(getwd())
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+util_dir <- file.path(root_dir, "util")
+data_dir <- file.path(root_dir,"data")
+input_dir <- file.path(root_dir,"input")
+analysis_dir <- file.path(root_dir,"analysis")
+intermediate_dir <- file.path(analysis_dir,"intermediate")
+
+source(paste(util_dir,"/is_cancer_who.R",sep = ""))
+source(paste(util_dir,"/create_disease_who_map.R",sep = ""))
+source(paste(util_dir,"/cancer_fuzzy_match.R",sep = ""))
+
+
+# Read CT Diseases
+ct_disease_df <- read.csv(paste(intermediate_dir,"/ct_disease_df.csv",sep=""))
+ct_disease_df<-ct_disease_df %>% dplyr::select(diseases)
+  
+who_general_words <- read_excel(paste(data_dir,"/who_cancer_key_words_general.xlsx",sep="")) # WHO Tumour List general (ADULT + PED)
+who_ped_words<-  read_excel(paste(data_dir,"/who_cancer_key_words_paediatric.xlsx",sep="")) # WHO Tumour List PED
+
+who_general_words$who_cancers<-tolower(who_general_words$who_cancers)
+who_ped_words$who_paediatric_cancers<-tolower(who_ped_words$who_paediatric_cancers)
+
+
+
+
+
+# Load Pedcan Hisologies from STJude, ALS and Cancer.net
+pedcan_histology <- read_excel(paste(data_dir,"/pediatric_cancer_list.xlsx",sep="")) # PedCan list from St Jude, Cancer.net and ALS
+pedcan_histology<- as.data.frame(c(pedcan_histology$Pedcan_Stjude,pedcan_histology$Pedcan_Alex,pedcan_histology$Pedcan_Cancernet))
+colnames(pedcan_histology)<-"Pediatric_Cancer"
+pedcan_histology <- pedcan_histology %>% drop_na()
+pedcan_histology$Pediatric_Cancer <- tolower(pedcan_histology$Pediatric_Cancer)
+pedcan_histology<-unique(pedcan_histology)
+
+
+cancer_search_terms <- "cancer|carcinoma|adenocarcinoma|tumor|lymphoma|blast|myeloma|melanoma|leukemia|astrocytoma|malignant|neoplasm|neoplasia|
+mesothelioma|ependymoma|glioma|thymoma|waldenstrom macroglobulinemia|myelodysplastic syndrome|polycythemia vera|myelofibrosis
+|myeloproliferative|sarcoma|gist-plus syndrome|macroglobulinemia|mycosis fungoides|sezary's disease|plasmacytoma"
+
+
+
+ind_cancer_search_term <- grepl(cancer_search_terms,ct_disease_df$diseases)
+
+ct_disease_df$cancer_search_term<-NA
+
+ct_disease_df$cancer_search_term[ind_cancer_search_term]<-"Yes"
+ct_disease_df$cancer_search_term[!ind_cancer_search_term]<-"No"
+
+
+ct_disease_df<-is_cancer_who(ct_disease_df,who_general_words)
+ct_disease_df<-ct_disease_df %>% dplyr::select(diseases,validated_cancer_tumor,cancer_search_term,is_cancer_who,PedCanTumor,Ped_Evidence)
+
+
+
