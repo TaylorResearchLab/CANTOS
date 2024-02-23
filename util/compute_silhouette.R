bb331ffcd01a4b352613845737edb1a4dcfd954b
Aditya Lahiri <lahiria@chop.edu>
2024-02-23 15:59:00 -0500

    update script for silo computation for edit dist

--- util/compute_silhouette.R
+++ util/compute_silhouette.R
@@ -6,11 +6,14 @@ compute_silhouette <- function (cluster_df,dist_mat){
   cluster_df$silhouette_score<-NA
   
   for (iter in 1:dim(cluster_df)[1]){
+    print(iter)
     disease_name <- cluster_df$Tumor_Names[iter]
     cluster_label <- cluster_df$SubsetCluster_IDs[iter]
     cluster_member_names <- cluster_df$Tumor_Names[which(cluster_df$SubsetCluster_IDs==cluster_label)]
       
     subset_dist_mat <- dist_mat %>% dplyr::filter(Tumor_Name %in% cluster_member_names) %>% dplyr::select(any_of(cluster_member_names))
+    
+    if(dim(subset_dist_mat)[1] > 1){
     
     ind_disease_name<- which(rownames(subset_dist_mat)==disease_name)
     a= subset_dist_mat[ind_disease_name,]
@@ -22,6 +25,7 @@ compute_silhouette <- function (cluster_df,dist_mat){
     subset_other_clust<-subset_other_clust[order(subset_other_clust$SubsetCluster_IDs),]
     
     other_clust_dist <- dist_mat %>% dplyr::filter(Tumor_Name %in% disease_name)
+    other_clust_dist<- other_clust_dist[,c(-dim(other_clust_dist)[2])]
     other_clust_dist<-t(other_clust_dist)
     colnames(other_clust_dist)<- "dx"
     other_clust_dist<-as.data.frame(other_clust_dist)
@@ -32,9 +36,10 @@ compute_silhouette <- function (cluster_df,dist_mat){
     b= min(d_clust$dx)
     
     silo= (b-a)/max(a,b)
+  }else if(dim(subset_dist_mat)[1] == 1){
+    silo=0
   }
-  
-
-  
+    cluster_df$silhouette_score[iter] <- silo 
+}
   return(cluster_df)
 }
