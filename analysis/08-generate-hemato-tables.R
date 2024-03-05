b3a2193da7a95d018814b1a49e0f5de96ff6f2bf
Aditya Lahiri <lahiria@chop.edu>
2024-03-04 23:35:30 -0500

    hemato analysis clustering may not be required

--- analysis/08-generate-hemato-tables.R
+++ analysis/08-generate-hemato-tables.R
@@ -51,17 +51,6 @@ registerDoParallel(cl)
 CalculateEuclideanDistance <- function(vect1, vect2) sqrt(sum((vect1 - vect2)^2)) 
 #stopCluster(cl)
 
-outer_who_final<-foreach(i = 1:dim(combined_embedding_df)[1], .combine = rbind) %dopar% { #12:10pm -
-  print(i)
-  #s <- apply(WHO_embedding_df[,2:1537],1,CalculateEuclideanDistance,vect2=combined_embedding_df[i,])
-  embedding_pairwise<- as.matrix(rbind(combined_embedding_df[i,],WHO_embedding_df[,2:1537]))
-  euclidean_dist <- as.matrix(dist(embedding_pairwise,method = "euclidean"))
-  d<-as.double(euclidean_dist[1,c(-1)])
-}
-colnames(outer_who_final)<-(WHO_embedding_df$Disease)
-rownames(outer_who_final)<-rownames(combined_embedding_df)
-
-
 
 
 ###### Lymphoma Luek analysis
@@ -77,10 +66,10 @@ affinity_cluster_hema_df <- affinity_cluster_df %>% dplyr::filter(Cluster_ID %in
 
 
 #sanity check 
-t1 <-t1 %>% filter(Var1 %in% hema_label)
-t2<-as.data.frame(table(affinity_cluster_hema_df$Cluster_ID))
-t3<- t1 %>% dplyr::left_join(t2, by="Var1")
-print(identical(t3[['Freq.x']],t3[['Freq.y']]))
+#t1 <-t1 %>% filter(Var1 %in% hema_label)
+#t2<-as.data.frame(table(affinity_cluster_hema_df$Cluster_ID))
+#t3<- t1 %>% dplyr::left_join(t2, by="Var1")
+#print(identical(t3[['Freq.x']],t3[['Freq.y']]))
 
 # compute distances to hemato 
 combined_embedding_hema_df <- combined_embedding_df %>% filter(rownames(combined_embedding_df) %in% affinity_cluster_hema_df$Tumor_Names)
@@ -215,6 +204,73 @@ for(iter in 1:length(hemato_cluster_labels)){
 }
 
 affinity_cluster_outlier<- affinity_cluster_outlier %>% dplyr::mutate(LOF_Outlier = case_when(LOF_Scores>1 ~ "Yes", TRUE ~ "No"))
+
+affinity_cluster_hema_df2<- affinity_cluster_hema_df %>% dplyr::select(Tumor_Names,assigned_class,Cluster_ID)
+affinity_cluster_hema_df2 <- affinity_cluster_hema_df2 %>% filter(!Tumor_Names %in% unique(c(NCIT_embedding_df$Disease,WHO_embedding_df$Disease)))
+affinity_cluster_hema_df2 <- affinity_cluster_hema_df2 %>% dplyr::left_join(affinity_cluster_outlier[,c(1)],by="Tumor_Names")
+
+
+
+
+
+##### ISOLATION FOR HEMA DF2
+affinity_cluster_outlier2<-affinity_cluster_hema_df2%>%dplyr::select(Tumor_Names,assigned_class)
+
+
+
+
+hemato_embedding<-affinity_cluster_outlier2 %>% dplyr::left_join(disease_transform,by="Tumor_Names")
+
+affinity_cluster_hema_df2$isolation_outlier_score<-NA
+
+hemato_cluster_labels <- unique(hemato_embedding$Cluster_ID)
+for(iter in 1:length(hemato_cluster_labels)){
+  cluster_label_current <- hemato_cluster_labels[iter]
+  set.seed(13)
+  hemato_embedding_subset <- hemato_embedding %>% dplyr::filter(Cluster_ID==cluster_label_current)
+  if(dim(hemato_embedding_subset)[1]>2){ # Need at least 2 data points to run isolation forest
+    model <- isolation.forest(hemato_embedding_subset[1:nrow(hemato_embedding_subset),3:ncol(hemato_embedding_subset)], ndim=3, ntrees=50, nthreads=1)
+    scores <- predict(model, hemato_embedding_subset[1:nrow(hemato_embedding_subset),3:ncol(hemato_embedding_subset)], type="score")
+    ind_clust <- which(affinity_cluster_hema_df2$Cluster_ID==cluster_label_current)
+    affinity_cluster_hema_df2$isolation_outlier_score[ind_clust]<-scores
+  }else{
+    ind_clust <- which(affinity_cluster_hema_df2$Cluster_ID==cluster_label_current)
+    affinity_cluster_outlier2$isolation_outlier_score[ind_clust]<-0
+  }
+}
+affinity_cluster_outlier2<- affinity_cluster_outlier2 %>% dplyr::mutate(Isolation_Outlier = case_when(isolation_outlier_score>0.5 ~ "Yes", TRUE ~ "No"))
+
+
+affinity_cluster_outlier$LOF_Scores<-NA
+lof_scores_minpts_list<-list()
+
+for(iter in 1:length(hemato_cluster_labels)){
+  cluster_label_current <- hemato_cluster_labels[iter]
+  ind_clust <- which(affinity_cluster_outlier$Cluster_ID==cluster_label_current)
+  lof_scores_minpts_list<-list()
+  
+  set.seed(13)
+  hemato_embedding_subset <- hemato_embedding %>% dplyr::filter(Cluster_ID==cluster_label_current)
+  if(dim(hemato_embedding_subset)[1]>2){ # Need at least 2 data points to run isolation forest
+    min_pts<- 2:(dim(hemato_embedding_subset)[1]-1)
+    for(iter_pts in min_pts){
+      lof_scores_minpts <- lof(hemato_embedding_subset[,3:137],iter_pts)
+      lof_scores_minpts_list[[as.character(iter_pts)]]<-lof_scores_minpts
+    }
+    lof_scores_minpts_list<- t(as.data.frame(lof_scores_minpts_list))
+    lof_scores_minpts_list_median<-apply(lof_scores_minpts_list,2,median)
+    affinity_cluster_outlier$LOF_Scores[ind_clust]<-lof_scores_minpts_list_median
+    
+  }else{
+    affinity_cluster_outlier$LOF_Scores[ind_clust]<-0
+  }
+  
+  
+}
+
+
+
+
 
 
 write.csv(affinity_cluster_hema_df,"hemato_tumor.csv")
