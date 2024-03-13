96f2535acc7492f7a4779c782a6897d8b93f20c0
Aditya Lahiri <lahiria@chop.edu>
2024-03-13 15:35:34 -0400

    adding distance computation script for v3

--- analysis/07B-annotate-cluster-result-V3-NCIT-WHO.R
+++ analysis/07B-annotate-cluster-result-V3-NCIT-WHO.R
@@ -28,6 +28,45 @@ source(paste(util_dir,"/cluster_label_assignment.R",sep=""))
 load(paste(intermediate_dir,"/affinity_cluster_v3_df.RData",sep=""))
 embedding_v3_large <- read.csv(paste(data_dir,"/embedding_tumor_names_text-embedding-3-large_embeddings.csv",sep=""))
 rownames(embedding_v3_large)<-embedding_v3_large$Disease
+
+NCIT_embedding_df <-read.csv(paste(data_dir,"/dt_input_file_6_dec/NCIT_Neoplasm_Core_terms_text-embedding-ada-002_embeddings.csv",sep=""))
+WHO_embedding_df <-read.csv(paste(data_dir,"/dt_input_file_6_dec/WHO_Only_terms_text-embedding-ada-002_embeddings.csv",sep=""))
+
+NCIT_embedding_df<-NCIT_embedding_df[c(-1),]
+WHO_embedding_df<-WHO_embedding_df[c(-1),]
+
+NCIT_Tumors <- tolower(NCIT_embedding_df$Disease)
+WHO_Tumors <- tolower(WHO_embedding_df$Disease)
+
+rm(NCIT_embedding_df,WHO_embedding_df)
+
+
+NCIT_embedding_df<- embedding_v3_large %>% dplyr::filter(Disease %in% NCIT_Tumors)
+WHO_embedding_df<- embedding_v3_large %>% dplyr::filter(Disease %in% WHO_Tumors)
 embedding_v3_large<-embedding_v3_large[,c(-1)]
+
+
+tumor_distances_df <- as.data.frame(matrix(nrow=dim(embedding_v3_large)[1],ncol = 5))
+colnames(tumor_distances_df)<- c("Tumor_Names","NCIT_Match","NCIT_Distance","WHO_Match","WHO_Distance")
+tumor_distances_df$Tumor_Names<- rownames(embedding_v3_large)
+
+
+cl <- makeCluster(6, outfile="")
+registerDoParallel(cl)
+
+CalculateEuclideanDistance <- function(vect1, vect2) sqrt(sum((vect1 - vect2)^2)) 
+
+outer_who_final<-foreach(i = 1:dim(embedding_v3_large)[1], .combine = rbind) %dopar% { #3:34 pm -
+  print(i)
+  embedding_pairwise<- as.matrix(rbind(embedding_v3_large[i,],WHO_embedding_df[,2:3073]))
+  euclidean_dist <- as.matrix(dist(embedding_pairwise,method = "euclidean"))
+  d<-as.double(euclidean_dist[1,c(-1)])
+}
+colnames(outer_who_final)<-(WHO_embedding_df$Disease)
+rownames(outer_who_final)<-rownames(embedding_v3_large)
+
+stopCluster(cl)
+
+save.image("script-7B.RData")
 
 
