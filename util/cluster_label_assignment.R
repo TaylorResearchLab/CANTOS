3868f1dd523f35cbf6513b2bd3efc6a2afb076c8
Aditya Lahiri <lahiria@chop.edu>
2024-03-23 20:15:22 -0400

    update cluster label assignment for who and ncit seperate lables

--- util/cluster_label_assignment.R
+++ util/cluster_label_assignment.R
@@ -1,18 +1,31 @@
 cluster_label_assignment <- function(affinity_cluster_nested){
   
   
-  affinity_cluster_nested$suggested_cluster_label <- NA
+  affinity_cluster_nested$who_cluster_label<- NA
+  affinity_cluster_nested$ncit_cluster_label<- NA
+  
+  
   subcluster_id_list<- unique(affinity_cluster_nested$Cluster_ID)
   
   for (iter in 1:length(subcluster_id_list)){
     
     cluster_id_affinity <- subcluster_id_list[iter]
     index_affinity <- which (affinity_cluster_nested$Cluster_ID==cluster_id_affinity)
-    totaling_table <- as.data.frame(table(affinity_cluster_nested$assigned_class[index_affinity]))
-    index_max<- which(totaling_table$Freq==max(totaling_table$Freq))
-    assigned_cluster_labels <- unique(as.character(totaling_table$Var1[index_max]))
+    #totaling_table <- as.data.frame(table(affinity_cluster_nested$assigned_class[index_affinity]))
     
-    affinity_cluster_nested$suggested_cluster_label[index_affinity]<- paste(assigned_cluster_labels,collapse = ";")
+    totaling_table_who <- as.data.frame(table(affinity_cluster_nested$WHO_Matches[index_affinity]))
+    totaling_table_ncit <- as.data.frame(table(affinity_cluster_nested$NCIT_Matches[index_affinity]))
+    
+    
+    index_max_who<- which(totaling_table_who$Freq==max(totaling_table_who$Freq))
+    index_max_ncit<- which(totaling_table_ncit$Freq==max(totaling_table_ncit$Freq))
+    
+    who_cluster_labels <- unique(as.character(totaling_table_who$Var1[index_max_who]))
+    ncit_cluster_labels <- unique(as.character(totaling_table_ncit$Var1[index_max_ncit]))
+    
+    affinity_cluster_nested$who_cluster_label[index_affinity]<- paste(who_cluster_labels,collapse = ";")
+    affinity_cluster_nested$ncit_cluster_label[index_affinity]<- paste(ncit_cluster_labels,collapse = ";")
+    
   }
 
   
