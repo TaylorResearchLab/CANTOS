d78f5bf288873f54bf8f9efd87cd7b6e90bf1fc6
Aditya Lahiri <lahiria@chop.edu>
2024-04-03 13:17:12 -0400

    need to find the closest WHO and NCIT matches for edit distances

--- analysis/12-generate-list-for-benchmark.R
+++ analysis/12-generate-list-for-benchmark.R
@@ -17,6 +17,7 @@ data_dir <- file.path(root_dir,"data")
 input_dir <- file.path(root_dir,"input")
 analysis_dir <- file.path(root_dir,"analysis")
 intermediate_dir <- file.path(analysis_dir,"intermediate")
+source(paste(util_dir,"/cluster_label_assignment.R",sep=""))
 
 # Read file v3
 affinity_cluster_v3_reassigned_df<-read.csv(paste(intermediate_dir,"/affinity_cluster_v3_reassigned_df.csv",sep=""))
@@ -35,14 +36,31 @@ nested_affinity_cluster_cosine <- read_csv("analysis/results/nested_affinity_clu
 nested_affinity_cluster_jw <- read_csv("analysis/results/nested_affinity_cluster_jw.csv")
 nested_affinity_cluster_lv <- read_csv("analysis/results/nested_affinity_cluster_lv.csv")
 
+nested_affinity_cluster_cosine<-nested_affinity_cluster_cosine[,c(-1)]
+nested_affinity_cluster_jw<-nested_affinity_cluster_jw[,c(-1)]
+nested_affinity_cluster_lv<-nested_affinity_cluster_lv[,c(-1)]
 
 # Find the WHO and NCIT closest matches 
 who_ncit_match_ADA2 <- affinity_cluster_ADA2_reassigned_df %>% dplyr::select(Tumor_Names,WHO_Matches,WHO_distance,NCIT_Matches,NCIT_distance) 
 who_ncit_match_v3 <- affinity_cluster_v3_reassigned_df %>% dplyr::select(Tumor_Names,WHO_Matches,WHO_distance,NCIT_Matches,NCIT_distance) 
 
-# Join them to Kmeans 
-kmeans_clust_result_embedding_ADA2<-kmeans_clust_result_embedding_ADA2 %>% dplyr::select(Tumor,cluster)
-kmeans_clust_result_embedding_V3<-kmeans_clust_result_embedding_V3 %>% dplyr::select(Tumor,cluster)
+# Join the matches to Kmeans 
+kmeans_clust_result_embedding_ADA2<-kmeans_clust_result_embedding_ADA2 %>% dplyr::select(Tumors,cluster)
+kmeans_clust_result_embedding_V3<-kmeans_clust_result_embedding_V3 %>% dplyr::select(Tumors,cluster)
 
 colnames(kmeans_clust_result_embedding_ADA2)<-c("Tumor_Names","Cluster_ID")
 colnames(kmeans_clust_result_embedding_V3)<-c("Tumor_Names","Cluster_ID")
+
+kmeans_clust_result_embedding_ADA2<-kmeans_clust_result_embedding_ADA2 %>% dplyr::left_join(who_ncit_match_ADA2,by="Tumor_Names")
+kmeans_clust_result_embedding_V3<-kmeans_clust_result_embedding_V3 %>% dplyr::left_join(who_ncit_match_v3,by="Tumor_Names")
+
+kmeans_clust_result_embedding_ADA2<- cluster_label_assignment(kmeans_clust_result_embedding_ADA2)
+kmeans_clust_result_embedding_V3<- cluster_label_assignment(kmeans_clust_result_embedding_V3)
+
+
+# Join the matches to affinity
+nested_affinity_cluster_cosine<-nested_affinity_cluster_cosine %>% dplyr::select(Tumor_Names,Cluster_ID)
+nested_affinity_cluster_jw<-nested_affinity_cluster_jw %>% dplyr::select(Tumor_Names,Cluster_ID)
+nested_affinity_cluster_lv<-nested_affinity_cluster_lv %>% dplyr::select(Tumor_Names,Cluster_ID)
+
+nested_affinity_cluster_cosine<-nested_affinity_cluster_cosine %>% dplyr::
