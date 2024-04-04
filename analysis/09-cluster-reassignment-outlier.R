27b55d0103373af46ccd3398f9eab3db0ba6b979
Aditya Lahiri <lahiria@chop.edu>
2024-04-04 15:28:17 -0400

    need to revise methods for cluster assignment

--- analysis/09-cluster-reassignment-outlier.R
+++ analysis/09-cluster-reassignment-outlier.R
@@ -81,6 +81,70 @@ for(iter in 1:dim(outlier_cluster_ada2)[1]){
   affinity_cluster_ADA2_reassigned_df<-affinity_cluster_ADA2_reassigned_df %>% rows_update(cluster_subset,by="Tumor_Names")
 }
 
+
+# cluster_repeats_table<- as.data.frame(table(affinity_cluster_ADA2_reassigned_df$Cluster_ID))
+# ind<- which(cluster_repeats_table$Freq>1)
+# repeating_clusters<-as.character(cluster_repeats_table$Var1[ind])
+# for(iter in 1: length(repeating_clusters)){
+#   current_cluster_id<- repeating_clusters[iter]
+#   ind_locations<- which(affinity_cluster_ADA2_reassigned_df$Cluster_ID==current_cluster_id)
+#   current_who_cluster_label <- unique(affinity_cluster_ADA2_reassigned_df$who_cluster_label[ind_locations])
+#   
+#   cluster_subset<-affinity_cluster_ADA2_reassigned_df%>% filter(Cluster_ID==current_cluster_id)
+#   
+#   table_who_matches<- as.data.frame(table(cluster_subset$WHO_Matches))
+#   table_who_matches$Var1<-as.character(table_who_matches$Var1)
+#   high_frequency_who_matches<- table_who_matches$Var1[which(table_who_matches$Freq>1)]
+#   flag_cluster_ID=0
+#   
+#   if(length(high_frequency_who_matches)==0){
+#     for(iter_repeats in ind_locations){
+#       counter=1
+#       affinity_cluster_ADA2_reassigned_df$Cluster_ID[iter_repeats]<- paste(affinity_cluster_ADA2_reassigned_df$Cluster_ID[iter_repeats],counter,sep = "+")
+#       affinity_cluster_ADA2_reassigned_df$who_cluster_label[iter_repeats]<-affinity_cluster_ADA2_reassigned_df$WHO_Matches[iter_repeats]
+#       counter=counter+1
+#       flag_cluster_ID=1
+#     }
+#   }else{
+#     high_frequency_who_matches<- table_who_matches$Var1[which(table_who_matches$Freq==max(table_who_matches$Freq))]
+#     if(length(high_frequency_who_matches)>1){
+#       print(current_cluster_id)
+#     }else{
+#       affinity_cluster_ADA2_reassigned_df$who_cluster_label[ind_locations]<-high_frequency_who_matches
+#     }
+# 
+#   }
+#   
+#   current_ncit_cluster_label <- unique(affinity_cluster_ADA2_reassigned_df$ncit_cluster_label[ind_locations])
+#   
+#   table_ncit_matches<- as.data.frame(table(cluster_subset$NCIT_Matches))
+#   table_ncit_matches$Var1<-as.character(table_ncit_matches$Var1)
+#   high_frequency_ncit_matches<- table_ncit_matches$Var1[which(table_ncit_matches$Freq>1)]
+#   
+#   if(length(high_frequency_ncit_matches)==0){
+#     for(iter_repeats in ind_locations){
+#       counter=1
+#       if(  flag_cluster_ID==0){
+#       affinity_cluster_ADA2_reassigned_df$Cluster_ID[iter_repeats]<- paste(affinity_cluster_ADA2_reassigned_df$Cluster_ID[iter_repeats],counter,sep = "+")
+#       flag_cluster_ID=1
+#       }
+#       affinity_cluster_ADA2_reassigned_df$ncit_cluster_label[iter_repeats]<-affinity_cluster_ADA2_reassigned_df$NCIT_Matches[iter_repeats]
+#       counter=counter+1
+#     }
+#   }else{
+#     high_frequency_ncit_matches<- table_ncit_matches$Var1[which(table_ncit_matches$Freq==max(table_ncit_matches$Freq))]
+#     if(length(high_frequency_ncit_matches)>1){
+#       print(current_cluster_id)
+#     }else{
+#       affinity_cluster_ADA2_reassigned_df$who_cluster_label[ind_locations]<-high_frequency_ncit_matches
+#     }
+#     affinity_cluster_ADA2_reassigned_df$ncit_cluster_label[ind_locations]<-high_frequency_ncit_matches
+#   }
+#   
+# }
+# 
+
+
 cluster_labels<-unique(affinity_cluster_ADA2_reassigned_df$Cluster_ID)
 cluster_labels<- as.data.frame(cbind(cluster_labels,c(1:length(cluster_labels))))
 colnames(cluster_labels)<-c("Cluster_ID","updated_ID")
