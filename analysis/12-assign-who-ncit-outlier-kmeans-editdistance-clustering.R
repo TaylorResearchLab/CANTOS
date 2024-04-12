d2b4c6fc0cb470a6e4fec9f143ef21d01294cb2e
Aditya Lahiri <lahiria@chop.edu>
2024-04-12 16:47:39 -0400

    adding validation samples

--- analysis/12-assign-who-ncit-outlier-kmeans-editdistance-clustering.R
+++ analysis/12-assign-who-ncit-outlier-kmeans-editdistance-clustering.R
@@ -430,11 +430,11 @@ colnames(conditions_data)[2]<-"Tumor_Names"
 
 
 # Random samples
-set.seed(13)
 affinity_cluster_v3_reassigned_df_short<- affinity_cluster_v3_reassigned_df_short %>% dplyr::left_join(conditions_data,by="Tumor_Names")
 affinity_cluster_v3_reassigned_df_short<-affinity_cluster_v3_reassigned_df_short %>%group_by(Tumor_Names) %>%summarise(nct_id=paste(nct_id, collapse=";"))
 affinity_cluster_v3_reassigned_df_short<-affinity_cluster_v3_reassigned_df_short%>%dplyr::filter(nct_id!="NA")
 
+set.seed(13)
 tumor_sample_df<-sample_n(affinity_cluster_v3_reassigned_df_short, 1000)
 tumor_sample_df<-tumor_sample_df %>% dplyr::left_join(affinity_cluster_ADA2_reassigned_df_short,by="Tumor_Names")%>%
   dplyr::left_join(kmeans_clust_result_embedding_V3_short,by="Tumor_Names") %>%
