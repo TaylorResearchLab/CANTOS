c04481cd4acf242ba305d989031383ba57259828
Aditya Lahiri <lahiria@chop.edu>
2024-05-20 14:37:36 -0400

    script to process and combine WHO Terms

---
+++ WHO_Term_Processing.R
@@ -0,0 +1,62 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(data.table)
+  library(ggplot2)
+  library(tidyverse)
+  library(stringi)
+  library(readxl)
+  library(openxlsx)
+})
+
+# Set the directories
+setwd(getwd())
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+util_dir <- file.path(root_dir, "util")
+data_dir <- file.path(root_dir,"data")
+input_dir <- file.path(root_dir,"input")
+analysis_dir <- file.path(root_dir,"analysis")
+intermediate_dir <- file.path(analysis_dir,"intermediate")
+result_dir <- file.path(analysis_dir,"results")
+
+# Load WHO Data 3rd edition
+excel_sheet_3rd_edition <- loadWorkbook((paste(data_dir,"/WHO_Tumors/3rd_edition/WHO_3rd_edition.xlsx",sep="")))
+sheetNames_3rd_edition <- sheets(excel_sheet_3rd_edition)
+df_3rd_edition<-data.frame()
+
+for(i in 1:length(sheetNames_3rd_edition))
+{
+  df_3rd_edition<- rbind(readWorkbook(excel_sheet_3rd_edition,sheet = i,colNames = FALSE),df_3rd_edition)
+}
+
+# Load WHO Data 4th edition
+excel_sheet_4th_edition <- loadWorkbook((paste(data_dir,"/WHO_Tumors/4th_edition/WHO_Tumor_4th_edition.xlsx",sep="")))
+sheetNames_4th_edition <- sheets(excel_sheet_4th_edition)
+
+df_4th_edition<-data.frame()
+for(i in 1:length(sheetNames_4th_edition))
+{
+  df_4th_edition<- rbind(readWorkbook(excel_sheet_4th_edition,sheet = i,colNames = FALSE),df_4th_edition)
+}
+
+# Load WHO Data 5th edition
+df_5th_edition <- read.csv((paste(data_dir,"/dt_input_file_6_dec/WHO_Only_terms.csv",sep="")))
+
+
+#rename the columns
+colnames(df_3rd_edition)<-"Tumor_Names"
+colnames(df_4th_edition)<-"Tumor_Names"
+colnames(df_5th_edition)<-"Tumor_Names"
+
+#lower case , and trim white spaces to left
+df_3rd_edition$Tumor_Names<- tolower(df_3rd_edition$Tumor_Names)
+df_4th_edition$Tumor_Names<- tolower(df_4th_edition$Tumor_Names)
+df_5th_edition$Tumor_Names<- tolower(df_5th_edition$Tumor_Names)
+
+df_3rd_edition$Tumor_Names<- str_trim(df_3rd_edition$Tumor_Names)
+df_4th_edition$Tumor_Names<- str_trim(df_4th_edition$Tumor_Names)
+df_5th_edition$Tumor_Names<- str_trim(df_5th_edition$Tumor_Names)
+
+# all who tumors combined
+
+WHO_Tumors <- rbind(df_3rd_edition,df_4th_edition,df_5th_edition)
+WHO_Tumors<- distinct(WHO_Tumors)
