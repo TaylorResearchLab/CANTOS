39f62c49768ee5e2d4d7f305510206f79ecfb9e1
Aditya Lahiri <lahiria@chop.edu>
2024-05-23 10:37:20 -0400

    adding all WHO terms

--- WHO_Term_Processing.R
+++ WHO_Term_Processing.R
@@ -84,13 +84,37 @@ write.xlsx(df_5th_edition,paste(data_dir,"/WHO_Tumors/intermediate/df_5th_editio
 write.xlsx(df_4th_edition,paste(data_dir,"/WHO_Tumors/intermediate/df_4th_edition_manual_edit.xlsx",sep=""))
 write.xlsx(df_3rd_edition,paste(data_dir,"/WHO_Tumors/intermediate/df_3rd_edition_manual_edit.xlsx",sep=""))
 
+
+
+#### Read post edit files
+WHO_df_3rd<- read.xlsx(paste(data_dir,"/WHO_Tumors/intermediate/df_3rd_edition_post_edit.xlsx",sep=""),colNames = FALSE)
+WHO_df_4th<- read.xlsx(paste(data_dir,"/WHO_Tumors/intermediate/df_4th_edition_post_edit.xlsx",sep=""),colNames = FALSE)
+WHO_df_5th<- read.xlsx(paste(data_dir,"/WHO_Tumors/intermediate/df_5th_edition_post_edit.xlsx",sep=""),colNames = FALSE)
+
+#lower case , and trim white spaces to left
+colnames(WHO_df_3rd)<- "Tumor_Names"
+colnames(WHO_df_4th)<- "Tumor_Names"
+colnames(WHO_df_5th)<- "Tumor_Names"
+
+WHO_df_3rd$Tumor_Names<- tolower(WHO_df_3rd$Tumor_Names)
+WHO_df_4th$Tumor_Names<- tolower(WHO_df_4th$Tumor_Names)
+WHO_df_5th$Tumor_Names<- tolower(WHO_df_5th$Tumor_Names)
+
+WHO_df_3rd$Tumor_Names<- str_trim(WHO_df_3rd$Tumor_Names)
+WHO_df_4th$Tumor_Names<- str_trim(WHO_df_4th$Tumor_Names)
+WHO_df_5th$Tumor_Names<- str_trim(WHO_df_5th$Tumor_Names)
+
 # all who tumors combined
 
-WHO_Tumors <- rbind(df_3rd_edition,df_4th_edition,df_5th_edition)
+WHO_Tumors <- rbind(WHO_df_3rd,WHO_df_4th,WHO_df_5th)
 WHO_Tumors<- distinct(WHO_Tumors)
 
 
 # Add editions to who tumors
-WHO_Tumors <- WHO_Tumors %>% mutate(edition_5th =case_when(Tumor_Names %in% df_5th_edition$Tumor_Names~"Yes", TRUE ~ "No"))
-WHO_Tumors <- WHO_Tumors %>% mutate(edition_4th =case_when(Tumor_Names %in% df_4th_edition$Tumor_Names~"Yes", TRUE ~ "No"))
-WHO_Tumors <- WHO_Tumors %>% mutate(edition_3rd =case_when(Tumor_Names %in% df_3rd_edition$Tumor_Names~"Yes", TRUE ~ "No"))
+WHO_Tumors <- WHO_Tumors %>% mutate(edition_5th =case_when(Tumor_Names %in% WHO_df_5th$Tumor_Names~"Yes", TRUE ~ "No"))
+WHO_Tumors <- WHO_Tumors %>% mutate(edition_4th =case_when(Tumor_Names %in% WHO_df_4th$Tumor_Names~"Yes", TRUE ~ "No"))
+WHO_Tumors <- WHO_Tumors %>% mutate(edition_3rd =case_when(Tumor_Names %in% WHO_df_3rd$Tumor_Names~"Yes", TRUE ~ "No"))
+
+# Write file for WHO Tumor
+write.xlsx(WHO_Tumors,paste(data_dir,"/WHO_Tumors/result/WHO_Tumor_all_edition.xlsx",sep=""))
+
