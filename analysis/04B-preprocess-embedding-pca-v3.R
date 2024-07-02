fce1de2d6ad62182349e65011491faf1b1394d63
Aditya Lahiri <lahiria@chop.edu>
2024-07-02 00:49:10 -0400

    uploading updates from script 04 A and 04 B

--- analysis/04B-preprocess-embedding-pca-v3.R
+++ analysis/04B-preprocess-embedding-pca-v3.R
@@ -35,18 +35,19 @@ NCIT_embedding_v3<-NCIT_embedding_v3[,c(-1)]
 
 combined_embeddings_df<-rbind(ct_tumor_embeddings_v3_df,WHO_Terms_V3,NCIT_embedding_v3)#19345
 combined_embeddings_df <- combined_embeddings_df%>%group_by(Tumor_Names) %>% summarise(across(everything(), list(mean)))#18284
-
-
-# PCA for v3 
+combined_embeddings_df<-as.data.frame(combined_embeddings_df)
 rownames(combined_embeddings_df)<-combined_embeddings_df$Tumor_Names
+colnames(combined_embeddings_df)<-colnames(NCIT_embedding_v3)
+
 combined_embeddings_df<-combined_embeddings_df[,c(-1)]
+# PCA for v3 
+
 results_v3 <- prcomp(combined_embeddings_df, scale = TRUE)
 eigs_v3 <- results_v3$sdev^2
 ff_v3<-rbind(SD = sqrt(eigs_v3),Proportion = eigs_v3/sum(eigs_v3), Cumulative = cumsum(eigs_v3)/sum(eigs_v3))
 # Check the number of components needed to capture 80% variance at least
 print(sum(ff_v3[2,1:185]))
 disease_transform_v3<-as.data.frame(-results_v3$x[,1:185])
-
 # Save this file "
 
 write.csv(disease_transform_v3,file =paste(intermediate_dir,"/disease_transform_pca_v3.csv",sep="") )
