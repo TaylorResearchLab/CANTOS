6bc004c867ca0e2318ce58d291cb62b9cfcffdc3
Aditya Lahiri <lahiria@chop.edu>
2024-07-01 20:15:33 -0400

    updates to 5B

--- analysis/05B-cluster-on-v3-embedding-Kmeans.R
+++ analysis/05B-cluster-on-v3-embedding-Kmeans.R
@@ -33,6 +33,7 @@ disease_transform_v3<- read.csv(paste(intermediate_dir,"/disease_transform_pca_v
 colnames(disease_transform_v3)[1]<-"Diseases"
 rownames(disease_transform_v3)<-disease_transform_v3$Diseases # Needed for AP Clust
 
+ncol= dim(disease_transform_v3)[2]
 
 
 # Set Seed
@@ -42,14 +43,14 @@ set.seed(13)
 # Peform Clustering 
 
 silhouette_score <- function(k){
-  km <- kmeans(disease_transform_v3[,2:178], centers = k, nstart=25)
-  ss <- silhouette(km$cluster, dist(disease_transform_v3[,2:136]))
+  km <- kmeans(disease_transform_v3[,2:ncol], centers = k, nstart=25)
+  ss <- silhouette(km$cluster, dist(disease_transform_v3[,2:ncol]))
   return(mean(ss[, 3]))
 }
 
-k <- c(10,100,500,1000,2000,3000,4000,4500,4600,4700,4800,4900,5000,5100,5200,5300,5400,
-       5500,5900, 6000,6050,6100,6200,6500,
-       7000,8000,9000,10000,11000,12000,13000,
+k <- c(10,500,1000,2000,3000,4000,5000,5500,5800,6000,6100,6200,6300,6400,
+       6500,6600,6700,6800,6900,
+       7000,7100,7200,7300,7400,7500,7600,7700,7800,7900,8000,9000,10000,11000,12000,13000,
        14000,15000,16000)
 
 avg_sil <- sapply(k, silhouette_score)
