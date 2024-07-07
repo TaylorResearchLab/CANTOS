4bbe46172e8363e4ada379ed0eeeca9a2276185f
adilahiri <lahiria@chop.edu>
2024-07-07 09:53:45 -0400

    fix file name in 7b

--- analysis/07B-annotate-cluster-result-V3-NCIT-WHO.R
+++ analysis/07B-annotate-cluster-result-V3-NCIT-WHO.R
@@ -31,10 +31,11 @@ load(paste(intermediate_dir,"/affinity_cluster_v3_df.RData",sep=""))
 load(paste(intermediate_dir,"/combined_embedding_v3_df.RData",sep=""))
 
 
-NCIT_embedding_df<-read.csv(paste(data_dir,"/WHO_Terms_ALL_V3.csv",sep=""))
-WHO_embedding_df<-read.csv(paste(data_dir,"/NCIT_Embeddings_V3.csv",sep=""))
-NCIT_embedding_df<-NCIT_embedding_df[,c(-1)]
+WHO_embedding_df<-read.csv(paste(data_dir,"/WHO_Terms_ALL_V3.csv",sep=""))
+NCIT_embedding_df<-read.csv(paste(data_dir,"/NCIT_Embeddings_V3.csv",sep=""))
+
 WHO_embedding_df<-WHO_embedding_df[,c(-1)]
+NCIT_embedding_df<-NCIT_embedding_df[,c(-1)]
 
 
 cl <- makeCluster(6, outfile="")
