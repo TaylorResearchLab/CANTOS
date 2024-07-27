1508584fd41b811302a3f1af4371ef0b2c224105
Aditya Lahiri <lahiria@chop.edu>
2024-07-27 15:12:07 -0400

    adding script2 5th edition results

--- 02-calculate-edit-distance-5thed.R
+++ 02-calculate-edit-distance-5thed.R
@@ -15,7 +15,7 @@ data_dir <- file.path(root_dir,"data")
 input_dir <- file.path(root_dir,"input")
 analysis_dir <- file.path(root_dir,"analysis")
 intermediate_dir <- file.path(analysis_dir,"intermediate")
-results_dir <- file.path(analysis_dir,"results")
+results_dir <- file.path(analysis_dir,"results_5th")
 
 # Load dissimalirity matrices
 load("/Users/lahiria/Desktop/MTP_Paper/temp/CT-Large-File-June21/dissimilarity_matrix_lv.RData")
@@ -38,26 +38,30 @@ ct_disease_annot_adult_ped_df<-read.csv(paste(input_dir,"/tumor_annotated_adult_
 ct_tumor_df<-ct_disease_annot_adult_ped_df%>%filter(validated_cancer_tumor=="Yes")
 ct_tumor_df<-ct_tumor_df[,c(-1)]
 
-tumor_names<- unique(c(ct_tumor_df$diseases,NCIT_Terms,WHO_Terms_5th$Tumor_Names))
+tumor_names_all<- unique(c(ct_tumor_df$diseases,NCIT_Terms,WHO_Terms_5th$Tumor_Names))
 
 dissimilarity_matrix_cosine<- dissimilarity_matrix_cosine %>% dplyr::filter(rownames(dissimilarity_matrix_cosine) %in% 
-                                                                              tumor_names )
+                                                                              tumor_names_all )
 dissimilarity_matrix_lv<- dissimilarity_matrix_lv %>% dplyr::filter(rownames(dissimilarity_matrix_lv) %in% 
-                                                                              tumor_names )
+                                                                      tumor_names_all )
 dissimilarity_matrix_jw<- dissimilarity_matrix_jw %>% dplyr::filter(rownames(dissimilarity_matrix_jw) %in% 
-                                                                              tumor_names )
+                                                                      tumor_names_all )
 
 
-dissimilarity_matrix_cosine<- dissimilarity_matrix_cosine %>%dplyr::select(any_of(tumor_names))
-dissimilarity_matrix_lv<- dissimilarity_matrix_lv %>%dplyr::select(any_of(tumor_names))
-dissimilarity_matrix_jw<- dissimilarity_matrix_jw %>% dplyr::select(any_of(tumor_names))
+dissimilarity_matrix_cosine<- dissimilarity_matrix_cosine %>%dplyr::select(any_of(tumor_names_all))
+dissimilarity_matrix_lv<- dissimilarity_matrix_lv %>%dplyr::select(any_of(tumor_names_all))
+dissimilarity_matrix_jw<- dissimilarity_matrix_jw %>% dplyr::select(any_of(tumor_names_all))
 
 
 df_tumor_names<- colnames(dissimilarity_matrix_lv)
 
+dissimilarity_matrix_cosine<-as.matrix(dissimilarity_matrix_cosine)
+dissimilarity_matrix_lv<-as.matrix(dissimilarity_matrix_lv)
+dissimilarity_matrix_jw<-as.matrix(dissimilarity_matrix_jw)
 
 
-# Find clusters of 
+
+# Find clusters of  lv
 cl <- makeCluster(25, outfile="")
 registerDoParallel(cl)
 cluster_results_lv<-foreach(iter=1:length(df_tumor_names),.combine=rbind) %do% {
@@ -72,3 +76,100 @@ cluster_results_lv <-cbind(cluster_results_lv,df_tumor_names)
 
 colnames(cluster_results_lv) <- c("cluster_lv","Tumors")
 cluster_results_lv <- cluster_results_lv %>% dplyr::select(Tumors,cluster_lv)
+
+### JW
+
+cluster_results_jw<-foreach(iter=1:length(df_tumor_names),.combine=rbind) %do% {
+  print(iter)
+  distance_clusters(dissimilarity_matrix_jw[iter,],cutoff = 0.0005,df_tumor_names)
+}
+
+cluster_results_jw<-as.data.frame(cluster_results_jw)
+rownames(cluster_results_jw)<-NULL
+cluster_results_jw <-cbind(cluster_results_jw,df_tumor_names)
+
+
+colnames(cluster_results_jw) <- c("cluster_jw","Tumors")
+cluster_results_jw <- cluster_results_jw %>%  dplyr::select(Tumors,cluster_jw)
+
+# Cosine
+
+cluster_results_cosine<-foreach(iter=1:length(df_tumor_names),.combine=rbind) %do% {
+  print(iter)
+  distance_clusters(dissimilarity_matrix_cosine[iter,],cutoff = 0.0005,df_tumor_names)
+}
+
+cluster_results_cosine<-as.data.frame(cluster_results_cosine)
+rownames(cluster_results_cosine)<-NULL
+cluster_results_cosine <-cbind(cluster_results_cosine,df_tumor_names)
+
+
+colnames(cluster_results_cosine) <- c("cluster_cosine","Tumors")
+cluster_results_cosine <- cluster_results_cosine %>%  dplyr::select(Tumors,cluster_cosine)
+
+
+# Number of clusters
+cluster_results_lv<-cluster_results_lv %>% mutate(cluster_members = str_count(cluster_lv, ";")+1)
+cluster_results_jw<-cluster_results_jw %>% mutate(cluster_members = str_count(cluster_jw, ";")+1)
+cluster_results_cosine<-cluster_results_cosine %>% mutate(cluster_members = str_count(cluster_cosine, ";")+1)  
+
+# filter by ct tumors
+cluster_results_lv_ct_filtered<- cluster_results_lv %>% filter(Tumors %in% ct_tumor_df$diseases)
+cluster_results_jw_ct_filtered<- cluster_results_jw %>% filter(Tumors %in% ct_tumor_df$diseases)
+cluster_results_cosine_ct_filtered<- cluster_results_cosine %>% filter(Tumors %in% ct_tumor_df$diseases)
+
+# add nct_id
+ct_tumor_df_disease_short <- ct_tumor_df %>% dplyr::select(nct_id,diseases)
+cluster_results_lv_ct_filtered <- cluster_results_lv_ct_filtered %>% left_join(ct_tumor_df_disease_short,by=c("Tumors"="diseases"))
+cluster_results_lv_ct_filtered <- cluster_results_lv_ct_filtered[,c(4,1:3)]
+
+
+cluster_results_jw_ct_filtered <- cluster_results_jw_ct_filtered %>% left_join(ct_tumor_df_disease_short,by=c("Tumors"="diseases"))
+cluster_results_jw_ct_filtered <- cluster_results_jw_ct_filtered[,c(4,1:3)]
+
+
+cluster_results_cosine_ct_filtered <- cluster_results_cosine_ct_filtered %>% left_join(ct_tumor_df_disease_short,by=c("Tumors"="diseases"))
+cluster_results_cosine_ct_filtered <- cluster_results_cosine_ct_filtered[,c(4,1:3)]
+
+
+# organize by alphabetical order
+cluster_results_lv_ct_filtered <- cluster_results_lv_ct_filtered[order(cluster_results_lv_ct_filtered$Tumors),]
+cluster_results_jw_ct_filtered <- cluster_results_jw_ct_filtered[order(cluster_results_jw_ct_filtered$Tumors),]
+cluster_results_cosine_ct_filtered <- cluster_results_cosine_ct_filtered[order(cluster_results_cosine_ct_filtered$Tumors),]
+
+
+
+# Null the row names
+rownames(cluster_results_lv_ct_filtered)<-NULL
+rownames(cluster_results_jw_ct_filtered)<-NULL
+rownames(cluster_results_cosine_ct_filtered)<-NULL
+
+
+# Show results with following tumors 
+benchmark_tumors <- c("b cell lymphoma", "neuroblastoma", "triple negative breast cancer",
+                      "unresectable lung carcinoma", "liposarcoma","cancer of the liver",
+                      "smoldering myeloma")
+
+display_table_benchmark <- cbind(cluster_results_lv_ct_filtered %>% filter(Tumors %in% benchmark_tumors) %>% dplyr::select(nct_id,Tumors,cluster_lv),
+                                 cluster_results_jw_ct_filtered %>% filter(Tumors %in%  benchmark_tumors) %>% dplyr::select(cluster_jw),
+                                 cluster_results_cosine_ct_filtered %>% filter(Tumors %in% benchmark_tumors) %>% dplyr::select(cluster_cosine)
+)
+
+
+display_table_benchmark <- display_table_benchmark %>% dplyr::select(nct_id,Tumors,cluster_lv,cluster_jw,cluster_cosine)
+
+# To organize from good to worse
+benchmark_tumors<- as.data.frame(benchmark_tumors)
+colnames(benchmark_tumors)<-"Tumors"
+display_table_benchmark <- benchmark_tumors %>% dplyr::left_join(display_table_benchmark,by="Tumors")
+
+display_table_benchmark <- display_table_benchmark %>% dplyr::select(nct_id,Tumors,cluster_lv,cluster_jw,cluster_cosine)
+
+
+# Stop the cluster
+stopCluster(cl)
+
+write.csv(cluster_results_lv_ct_filtered,paste(results_dir,"/cluster_lv.csv",sep=""))
+write.csv(cluster_results_jw_ct_filtered,paste(results_dir,"/cluster_jw.csv",sep=""))
+write.csv(cluster_results_cosine_ct_filtered,paste(results_dir,"/cluster_cosine.csv",sep=""))
+write.csv(display_table_benchmark,paste(results_dir,"/edit_distance_bench_mark.csv",sep=""))
