4fab24abedcf4b4fc1daf3f19f168845dba9d8ee
adilahiri <lahiria@chop.edu>
2024-08-04 14:31:14 -0400

    adding 7B analysis result after running on server

--- analysis/07B-annotate-cluster-result-V3-NCIT-WHO-5thed.R
+++ analysis/07B-annotate-cluster-result-V3-NCIT-WHO-5thed.R
@@ -51,8 +51,112 @@ outer_who_final<-foreach(i = 1:dim(combined_embeddings_df)[1], .combine = rbind)
 colnames(outer_who_final)<-(WHO_embedding_df$Tumor_Names)
 rownames(outer_who_final)<-rownames(combined_embeddings_df)
 
-combined_embeddings_df
+combined_embedding<-combined_embeddings_df
 # 
-start
+# while(stop <501){
+#   outer_who_final<-foreach(i = start:stop, .combine = rbind) %dopar% { #12:10pm -
+#     print(i)
+#     embedding_pairwise<- as.matrix(rbind(combined_embeddings_df[i,],WHO_embedding_df[,2:ncol(WHO_embedding_df)]))
+#     euclidean_dist <- as.matrix(dist(embedding_pairwise,method = "euclidean"))
+#     d<-as.double(euclidean_dist[1,c(-1)])
+#   }
+#   
+#   tmp<-rbind(tmp,outer_who_final)
+#   rm(outer_who_final)
+#   start=stop+1
+#   stop=start+199
+#   save.image(file = "script7b-5thed_aug1_1146pm.RData")
+#   stopCluster(cl)
+#   rm(cl)
+#   cl <- makeCluster(50, outfile="")
+#   registerDoParallel(cl)
+#   
+# }
 
-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
+outer_NCIT_final<-foreach(i = 1:dim(combined_embeddings_df)[1], .combine = rbind) %dopar% { #2:03 pm Friday
+  print(i)
+  embedding_pairwise<- as.matrix(rbind(combined_embeddings_df[i,],NCIT_embedding_df[,2:3073]))
+  euclidean_dist <- as.matrix(dist(embedding_pairwise,method = "euclidean"))
+  d<-as.double(euclidean_dist[1,c(-1)])
+}
+colnames(outer_NCIT_final)<-(NCIT_embedding_df$Tumor_Names)
+rownames(outer_NCIT_final)<-rownames(combined_embeddings_df)
+
+
+
+# while(stop <1001){
+#   outer_NCIT_final<-foreach(i = start:stop, .combine = rbind) %dopar% { #2:03 pm Friday
+#     print(i)
+#     embedding_pairwise<- as.matrix(rbind(combined_embeddings_df[i,],NCIT_embedding_df[,2:3073]))
+#     euclidean_dist <- as.matrix(dist(embedding_pairwise,method = "euclidean"))
+#     d<-as.double(euclidean_dist[1,c(-1)])
+#   }
+# 
+#   tmp<-rbind(tmp,outer_NCIT_final)
+#   rm(outer_NCIT_final)
+#   start=stop+1
+#   stop=start+199
+#   save.image(file = "script7b-5thed_aug1_1146pm.RData")
+#   stopCluster(cl)
+#   rm(cl)
+#   cl <- makeCluster(50, outfile="")
+#   registerDoParallel(cl)
+# 
+# }
+
+
+######################
+index_min_who <- as.matrix(apply(outer_who_final, 1, which.min))
+
+who_match_df <- cbind(rownames(outer_who_final))
+
+colnames(who_match_df)<-"Tumor_Names"
+who_match_df <-as.data.frame(who_match_df)
+
+who_match_df$WHO_Matches<- NA
+who_match_df$WHO_distance<-NA
+
+for (iter in 1: dim(who_match_df)[1]){
+  
+  who_match_df$WHO_Matches[iter] <- colnames(outer_who_final)[index_min_who[iter]]
+  who_match_df$WHO_distance[iter]<-outer_who_final[iter,index_min_who[iter]]
+  
+}
+
+
+
+index_min_NCIT <- as.matrix(apply(outer_NCIT_final, 1, which.min))
+
+NCIT_match_df <- cbind(rownames(outer_NCIT_final))
+
+colnames(NCIT_match_df)<-"Tumor_Names"
+NCIT_match_df <-as.data.frame(NCIT_match_df)
+
+NCIT_match_df$NCIT_Matches<- NA
+NCIT_match_df$NCIT_distance<-NA
+
+for (iter in 1: dim(NCIT_match_df)[1]){
+  
+  NCIT_match_df$NCIT_Matches[iter] <- colnames(outer_NCIT_final)[index_min_NCIT[iter]]
+  NCIT_match_df$NCIT_distance[iter]<-outer_NCIT_final[iter,index_min_NCIT[iter]]
+  
+}
+
+
+########
+affinity_cluster_v3_df<- affinity_cluster_v3_df %>% dplyr::left_join(who_match_df,by="Tumor_Names")
+affinity_cluster_v3_df<- affinity_cluster_v3_df %>% dplyr::left_join(NCIT_match_df,by="Tumor_Names")
+
+
+
+affinity_cluster_v3_df<- cluster_label_assignment_refined(affinity_cluster_v3_df)
+
+tumor_id<- read.csv(paste(data_dir,"/Tumor_NCT_ID.csv",sep=""))
+tumor_id<- tumor_id[,c(-1)]
+affinity_cluster_v3_df<-affinity_cluster_v3_df%>%left_join(tumor_id,by="Tumor_Names")
+affinity_cluster_v3_df<-affinity_cluster_v3_df[,c(9,1:8)]
+
+
+##
+write.csv(affinity_cluster_v3_df,paste(intermediate_dir,"/affinity_cluster_v3_df_5thed.csv",sep=""))
+save.image(file = "script7b-5thed_aug4.RData")
