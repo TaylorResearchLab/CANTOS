fe39bfef2303866bc866703818045e5f271ec83c
Aditya Lahiri <lahiria@chop.edu>
2024-11-12 11:25:01 -0500

    update and add data table

--- analysis/14-compute-euclidean-v3-threshold.R
+++ analysis/14-compute-euclidean-v3-threshold.R
@@ -36,3 +36,29 @@ tumor_5th_edition<-tumor_5th_edition %>% dplyr::select(nct_id,Tumor_Names,ground
                                                        euclidean_dist_v3,valid_euclidean_dist_v3)
 tumor_all_edition<-tumor_all_edition %>% dplyr::select(nct_id,Tumor_Names,ground_truth_val,ground_truth,
                                                        euclidean_dist_v3,valid_euclidean_dist_v3)
+
+
+# Load 5th edition distance
+affinity_cluster_v3_reassigned_5thed_df<-read.csv(paste(intermediate_dir_5th,"/affinity_cluster_v3_reassigned_df_5thed.csv",sep=""))
+WHO_5th_edition<-affinity_cluster_v3_reassigned_5thed_df %>% dplyr::select(Tumor_Names,WHO_Matches,WHO_distance)
+
+
+# Load all edition distance
+affinity_cluster_v3_reassigned_df<-read.csv(paste(intermediate_dir,"/affinity_cluster_v3_reassigned_df.csv",sep=""))
+WHO_all_edition<-affinity_cluster_v3_reassigned_df %>% dplyr::select(Tumor_Names,WHO_Matches,WHO_distance)
+
+
+# Join 5th edition data
+tumor_5th_edition<- tumor_5th_edition %>% dplyr::left_join(WHO_5th_edition,by="Tumor_Names")
+
+#Join all edition data
+tumor_all_edition<- tumor_all_edition %>% dplyr::left_join(WHO_all_edition,by="Tumor_Names")
+
+
+
+# Remove cases where there were no ground truths (NF = Not Found)
+tumor_5th_edition<- tumor_5th_edition %>%filter(ground_truth !="NF")
+tumor_all_edition<- tumor_all_edition %>%filter(ground_truth !="NF")
+
+
+# data table for 5th edition Euclidean V3 distance
