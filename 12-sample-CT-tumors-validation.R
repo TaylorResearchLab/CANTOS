9f8212e11004c41a29fe710290989b487fac583a
Aditya Lahiri <lahiria@chop.edu>
2025-01-10 15:31:06 -0500

    update script for validation

---
+++ 12-sample-CT-tumors-validation.R
@@ -0,0 +1,36 @@
+# Load libraries
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(data.table)
+  library(ggplot2)
+  library(tidyverse)
+  library(stringi)
+  library(qdapRegex)
+  library(jsonlite)
+  library(httr)
+  library(biomaRt)
+  library(ghql)
+  library(readxl)
+  library(rollama)
+  library(doParallel)
+  library(foreach)
+  library(stringdist)
+  library(rdist)
+  library(readr)
+})
+setwd(getwd())
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+util_dir <- file.path(root_dir, "util")
+data_dir <- file.path(root_dir,"data")
+input_dir <- file.path(root_dir,"data","input")
+analysis_dir <- file.path(root_dir,"analysis")
+
+
+# Load results to draw 1600 samples:
+WHO_Results_all<- read.csv(paste(analysis_dir,"/results/WHO_Results_all_ose.csv",sep=""))
+WHO_Results_5th<- read.csv(paste(analysis_dir,"/results_5th/WHO_Results_5thed_ose.csv",sep=""))
+
+WHO_Results_all<-WHO_Results_all[,c(-1)]
+WHO_Results_5th<-WHO_Results_5th[,c(-1)]
+
+
