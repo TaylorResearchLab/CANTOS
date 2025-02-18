0945855f2ad47d9502a6fdee042bc03ad6321505
Aditya Lahiri <lahiria@chop.edu>
2025-02-18 14:52:26 -0500

    Update Mutual Information analysis

--- correlation_analysis.R
+++ correlation_analysis.R
@@ -12,6 +12,7 @@ suppressPackageStartupMessages({
   library(dbscan)
   library(isotree)
   library(infotheo)
+  library(combinat)
 })
 
 # Set the directories
@@ -42,7 +43,7 @@ tumor_5thed_gt<-tumor_5thed_gt%>%filter(ground_truth %in% c("G","MG","G-Manual")
 data_5th<- tumor_5thed_gt[,seq(6,76,2)]
 data_all<- tumor_all_gt[,seq(6,76,2)]
 
-col_select <- c("valid_euclidean_dist_v3","valid_euclidean_dist_ada2","valid_af_ad2","valid_kmeans_v3",
+col_select <- c("valid_euclidean_dist_v3","valid_euclidean_dist_ada2","valid_af_ad2","valid_af_v3","valid_kmeans_v3",
                 "valid_kmeans_ada2","valid_euclidean_dist_MiniLM_L6_v2","valid_euclidean_mpnet_base",
                 "valid_euclidean_dist_e5_large","valid_euclidean_dist_MiniLM_L12_v2","valid_euclidean_dist_nomic")
 
@@ -50,95 +51,12 @@ col_select <- c("valid_euclidean_dist_v3","valid_euclidean_dist_ada2","valid_af_
 data_5th<-data_5th%>%dplyr::select(col_select)
 data_all<-data_all%>%dplyr::select(col_select)
 
-cor_mat_5th = cor(data_5th)
-cor_mat_all = cor(data_all)
-
-
-
-which(cor_mat_5th==min(cor_mat_5th),arr.ind = TRUE)
-which(cor_mat_all==min(cor_mat_all),arr.ind = TRUE)
-
-combined_pred_MiniLM_L12_kmeans_v3_5th<-tumor_5thed_gt%>%dplyr::select(nct_id,Tumor_Names,ground_truth,ground_truth_val,
-                                                                       kmeans_v3,valid_kmeans_v3,euclidean_dist_MiniLM_L12_v2,
-                                                                       valid_euclidean_dist_MiniLM_L12_v2) 
-
-combined_pred_MiniLM_L12_kmeans_v3_5th$combined_predictions<-NA
-combined_pred_MiniLM_L12_kmeans_v3_5th$valid_combined_predictions<-NA
-
-for (iter in 1:nrow(combined_pred_MiniLM_L12_kmeans_v3_5th)){
-  if(combined_pred_MiniLM_L12_kmeans_v3_5th$valid_kmeans_v3[iter]==combined_pred_MiniLM_L12_kmeans_v3_5th$valid_euclidean_dist_MiniLM_L12_v2[iter]){
-    combined_pred_MiniLM_L12_kmeans_v3_5th$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_5th$euclidean_dist_MiniLM_L12_v2[iter]
-  }else if(combined_pred_MiniLM_L12_kmeans_v3_5th$valid_kmeans_v3[iter]!=combined_pred_MiniLM_L12_kmeans_v3_5th$valid_euclidean_dist_MiniLM_L12_v2[iter]){
-    if(combined_pred_MiniLM_L12_kmeans_v3_5th$valid_kmeans_v3[iter]==1){
-      combined_pred_MiniLM_L12_kmeans_v3_5th$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_5th$kmeans_v3[iter]
-    }else if(combined_pred_MiniLM_L12_kmeans_v3_5th$valid_euclidean_dist_MiniLM_L12_v2[iter]==1){
-      combined_pred_MiniLM_L12_kmeans_v3_5th$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_5th$euclidean_dist_MiniLM_L12_v2[iter]
-    }
-  }
-  ground_truths<- combined_pred_MiniLM_L12_kmeans_v3_5th$ground_truth_val[iter]
-  ground_truths<-unique(unlist(strsplit(ground_truths,";")))
-  
-  if(combined_pred_MiniLM_L12_kmeans_v3_5th$combined_predictions[iter] %in% ground_truths){
-    combined_pred_MiniLM_L12_kmeans_v3_5th$valid_combined_predictions[iter]=1
-  }else{
-    combined_pred_MiniLM_L12_kmeans_v3_5th$valid_combined_predictions[iter]=0
-  }
-  
-  
-}
-
-print(sum(combined_pred_MiniLM_L12_kmeans_v3_5th$valid_combined_predictions)/1044)
-
-
-
-
-
-
-
-######
-
-combined_pred_MiniLM_L12_kmeans_v3_all<-tumor_all_gt%>%dplyr::select(nct_id,Tumor_Names,ground_truth,ground_truth_val,
-                                                                       kmeans_v3,valid_kmeans_v3,euclidean_dist_MiniLM_L12_v2,
-                                                                       valid_euclidean_dist_MiniLM_L12_v2)
-
-combined_pred_MiniLM_L12_kmeans_v3_all$combined_predictions<-NA
-combined_pred_MiniLM_L12_kmeans_v3_all$valid_combined_predictions<-NA
-
-for (iter in 1:nrow(combined_pred_MiniLM_L12_kmeans_v3_all)){
-  if(combined_pred_MiniLM_L12_kmeans_v3_all$valid_kmeans_v3[iter]==combined_pred_MiniLM_L12_kmeans_v3_all$valid_euclidean_dist_MiniLM_L12_v2[iter]){
-    combined_pred_MiniLM_L12_kmeans_v3_all$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_all$euclidean_dist_MiniLM_L12_v2[iter]
-  }else if(combined_pred_MiniLM_L12_kmeans_v3_all$valid_kmeans_v3[iter]!=combined_pred_MiniLM_L12_kmeans_v3_all$valid_euclidean_dist_MiniLM_L12_v2[iter]){
-    if(combined_pred_MiniLM_L12_kmeans_v3_all$valid_kmeans_v3[iter]==1){
-      combined_pred_MiniLM_L12_kmeans_v3_all$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_all$kmeans_v3[iter]
-    }else if(combined_pred_MiniLM_L12_kmeans_v3_all$valid_euclidean_dist_MiniLM_L12_v2[iter]==1){
-      combined_pred_MiniLM_L12_kmeans_v3_all$combined_predictions[iter]<-combined_pred_MiniLM_L12_kmeans_v3_all$euclidean_dist_MiniLM_L12_v2[iter]
-    }
-  }
-  ground_truths<- combined_pred_MiniLM_L12_kmeans_v3_all$ground_truth_val[iter]
-  ground_truths<-unique(unlist(strsplit(ground_truths,";")))
-  
-  if(combined_pred_MiniLM_L12_kmeans_v3_all$combined_predictions[iter] %in% ground_truths){
-    combined_pred_MiniLM_L12_kmeans_v3_all$valid_combined_predictions[iter]=1
-  }else{
-    combined_pred_MiniLM_L12_kmeans_v3_all$valid_combined_predictions[iter]=0
-  }
-  
-  
-}
-
-print(sum(combined_pred_MiniLM_L12_kmeans_v3_all$valid_combined_predictions)/1118)
-
-
-
-
-
-### Repeat analysis with MI
-
-MI_mat_5th <- matrix(0, nrow = 10, ncol = 10)
+num_methods<-length(col_select)
 
 # Compute pairwise Mutual Information
-for (i in 1:10) {
-  for (j in i:10) {
+MI_mat_5th <- matrix(0, nrow = num_methods, ncol = num_methods)
+for (i in 1:num_methods) {
+  for (j in i:num_methods) {
     mi_score <- mutinformation(data_5th[[i]], data_5th[[j]])
     MI_mat_5th[i, j] <- mi_score
     MI_mat_5th[j, i] <- mi_score  # Fill both halves since it's symmetric
@@ -148,11 +66,11 @@ colnames(MI_mat_5th) <- colnames(data_5th)
 rownames(MI_mat_5th) <- colnames(data_5th)
 
 
-MI_mat_all <- matrix(0, nrow = 10, ncol = 10)
+MI_mat_all <- matrix(0, nrow = num_methods, ncol = num_methods)
 
 # Compute pairwise Mutual Information
-for (i in 1:10) {
-  for (j in i:10) {
+for (i in 1:num_methods) {
+  for (j in i:num_methods) {
     mi_score <- mutinformation(data_all[[i]], data_all[[j]])
     MI_mat_all[i, j] <- mi_score
     MI_mat_all[j, i] <- mi_score  # Fill both halves since it's symmetric
@@ -164,5 +82,32 @@ rownames(MI_mat_all) <- colnames(data_all)
 
 which(MI_mat_5th==min(MI_mat_5th),arr.ind = TRUE)
 which(MI_mat_all==min(MI_mat_all),arr.ind = TRUE)  
+method_combinations <- combn(col_select, 3, simplify = FALSE)
 
-# Same as MiniLM_L12_v2 and Kmean_v3
+compute_avg_mi <- function(methods, mi_matrix) {
+  sub_matrix <- mi_matrix[methods, methods]  # Extract sub-matrix for selected methods
+  avg_mi <- mean(sub_matrix[lower.tri(sub_matrix)])  # Average off-diagonal mutual information
+  return(avg_mi)
+}
+
+mi_scores_5th <- sapply(method_combinations, function(combo) compute_avg_mi(combo, MI_mat_5th))
+mi_scores_all <- sapply(method_combinations, function(combo) compute_avg_mi(combo, MI_mat_all))
+
+collapsed_string <- sapply(method_combinations, function(x) paste(x, collapse = "+"))
+
+
+mi_scores_5th<-as.data.frame(cbind(collapsed_string,mi_scores_5th))
+mi_scores_all<-as.data.frame(cbind(collapsed_string,mi_scores_all))
+
+colnames(mi_scores_5th)<- c("methods","mi_score")
+colnames(mi_scores_all)<- c("methods","mi_score")
+
+mi_scores_5th<-mi_scores_5th[order(mi_scores_5th$mi_score),]
+mi_scores_all<-mi_scores_all[order(mi_scores_all$mi_score),]
+rownames(mi_scores_5th)<-NULL
+rownames(mi_scores_all)<-NULL
+
+print(paste("Best diverse combination (lowest mutual information):", mi_scores_5th$methods[1]))
+print(paste("Best diverse combination (lowest mutual information):", mi_scores_all$methods[1]))
+
+
