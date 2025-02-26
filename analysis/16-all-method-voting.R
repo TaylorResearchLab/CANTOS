727d20b8c07cddc23ffc17493b8528f4db4c47e2
Aditya Lahiri <lahiria@chop.edu>
2025-02-26 15:17:04 -0500

    update methods with all voting results

--- analysis/16-all-method-voting.R
+++ analysis/16-all-method-voting.R
@@ -31,7 +31,6 @@ col_select <- c("valid_euclidean_dist_v3","valid_euclidean_dist_ada2","valid_af_
                 "valid_kmeans_ada2","valid_euclidean_dist_MiniLM_L6_v2","valid_euclidean_mpnet_base",
                 "valid_euclidean_dist_e5_large","valid_euclidean_dist_MiniLM_L12_v2","valid_euclidean_dist_nomic")
 
-source(paste(util_dir,"/compute_mi_pairwise.R",sep = ""))
 
 tumor_all_gt<-read.csv(paste(result_dir,"/tumor_manually_validated_all.csv",sep = ""))
 tumor_5thed_gt<-read.csv(paste(result_dir_5th,"/tumor_manually_validated_5th.csv",sep = ""))
@@ -41,37 +40,77 @@ tumor_5thed_gt<-tumor_5thed_gt[,c(-1)]
 col_select2<-col_select
 col_select2 <- gsub("valid_","",col_select2)
 col_select2[3]<-"af_ada2"  
-tumor_5thed_gt2<-tumor_5thed_gt%>%dplyr::select(nct_id,Tumor_Names,ground_truth,ground_truth_val,col_select,col_select2)
+tumor_5thed_gt<-tumor_5thed_gt%>%dplyr::select(nct_id,Tumor_Names,ground_truth,ground_truth_val,col_select,col_select2)
 
-tumor_5thed_gt2$combined_predictions<-NA
-tumor_5thed_gt2$valid_combined_predictions<-NA
+tumor_5thed_gt$combined_predictions<-NA
+tumor_5thed_gt$valid_combined_predictions<-NA
 
-for(iter in 1:nrow(tumor_5thed_gt2)){
-  ground_truths<- tumor_5thed_gt2$ground_truth_val[iter]
+
+tumor_all_gt<-tumor_all_gt%>%dplyr::select(nct_id,Tumor_Names,ground_truth,ground_truth_val,col_select,col_select2)
+
+tumor_all_gt$combined_predictions<-NA
+tumor_all_gt$valid_combined_predictions<-NA
+
+for(iter in 1:nrow(tumor_5thed_gt)){
+  ground_truths<- tumor_5thed_gt$ground_truth_val[iter]
   ground_truths<-unique(unlist(strsplit(ground_truths,";")))
   
-  list_std<-as.character(tumor_5thed_gt2[iter,16:26])
+  list_std<-as.character(tumor_5thed_gt[iter,16:26])
   
   table_pred<-as.data.frame(table(list_std))
   most_frequent <- as.character(table_pred$Var1[table_pred$Freq== max(table_pred$Freq)])
   
   if(length(most_frequent)==1){
-    tumor_5thed_gt2$combined_predictions[iter]=most_frequent
+    tumor_5thed_gt$combined_predictions[iter]=most_frequent
   }else{
-    tumor_5thed_gt2$combined_predictions[iter]=tumor_5thed_gt2$euclidean_dist_MiniLM_L12_v2[iter]
+    tumor_5thed_gt$combined_predictions[iter]=tumor_5thed_gt$euclidean_dist_MiniLM_L12_v2[iter]
   }
   
-  if(tumor_5thed_gt2$combined_predictions[iter] %in% ground_truths){
-    tumor_5thed_gt2$valid_combined_predictions[iter]=1
+  if(tumor_5thed_gt$combined_predictions[iter] %in% ground_truths){
+    tumor_5thed_gt$valid_combined_predictions[iter]=1
   }else{
-    tumor_5thed_gt2$valid_combined_predictions[iter]=0
+    tumor_5thed_gt$valid_combined_predictions[iter]=0
   }
   
   
   
 }
 
-print(sum(tumor_5thed_gt2$valid_combined_predictions)/nrow(tumor_5thed_gt2))
+print(sum(tumor_5thed_gt$valid_combined_predictions)/nrow(tumor_5thed_gt))
+
+
+
+
+####
+
+for(iter in 1:nrow(tumor_all_gt)){
+  ground_truths<- tumor_all_gt$ground_truth_val[iter]
+  ground_truths<-unique(unlist(strsplit(ground_truths,";")))
+  
+  list_std<-as.character(tumor_all_gt[iter,16:26])
+  
+  table_pred<-as.data.frame(table(list_std))
+  most_frequent <- as.character(table_pred$Var1[table_pred$Freq== max(table_pred$Freq)])
+  
+  if(length(most_frequent)==1){
+    tumor_all_gt$combined_predictions[iter]=most_frequent
+  }else{
+    tumor_all_gt$combined_predictions[iter]=tumor_all_gt$euclidean_dist_v3[iter]
+  }
+  
+  if(tumor_all_gt$combined_predictions[iter] %in% ground_truths){
+    tumor_all_gt$valid_combined_predictions[iter]=1
+  }else{
+    tumor_all_gt$valid_combined_predictions[iter]=0
+  }
+  
+  
+  
+}
+
+print(sum(tumor_all_gt$valid_combined_predictions)/nrow(tumor_all_gt))
+
+
 
 
 
