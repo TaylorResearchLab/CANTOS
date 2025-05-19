6cb32a64f2310da249db362afcf8c14204e01b01
Aditya Lahiri <lahiria@chop.edu>
2025-05-18 21:04:36 -0400

    upload scripts for euclidean + embeddings

--- analysis/run-time-analysis/run.sh
+++ analysis/run-time-analysis/run.sh
@@ -10,7 +10,21 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 #Rscript cosine_match.R
 #Rscript jw_match.R
 
-Rscript Medllama2_Euclidean.R
-Rscript llama32_Euclidean.R
-Rscript Phi4_Euclidean.R
-Rscript llama33_70b_Euclidean.R
+#Rscript Medllama2_Euclidean.R
+#Rscript llama32_Euclidean.R
+#Rscript Phi4_Euclidean.R
+#Rscript llama33_70b_Euclidean.R
+
+#Rscript e5_large_Euclidean.R
+
+#Rscript e5_large_v2_Euclidean.R
+#Rscript cohere_euclidean.R
+
+#Rscript nomic_Euclidean.R
+#Rscript roberta_Euclidean.R
+#Rscript modernBERT_Euclidean.R
+
+#Rscript deepseek_8b_Euclidean.R
+Rscript BioGPT_Euclidean.R
+Rscript all-mpnet-base-v2_Euclidean.R
+Rscript clinicalBERT_Euclidean.R
