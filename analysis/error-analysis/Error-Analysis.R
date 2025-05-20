09640c90aeabd93761f0bb62a98c53f1534f4a5a
Aditya Lahiri <lahiria@chop.edu>
2025-05-20 16:53:16 -0400

    update error analysis bar plots

--- analysis/error-analysis/Error-Analysis.R
+++ analysis/error-analysis/Error-Analysis.R
@@ -3,32 +3,21 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 library(tidyverse)
-library(viridis)
 
-sheet_names <- excel_sheets("categorized_tumor_errors_v7.xlsx")
+setwd(getwd())
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+util_dir <- file.path(root_dir, "util")
+data_dir <- file.path(root_dir,"data")
+input_dir <- file.path(root_dir,"input")
+analysis_dir <- file.path(root_dir,"analysis")
 
-# Read all sheets into a named list of data frames
-all_sheets <- lapply(sheet_names, read_excel, path = "categorized_tumor_errors_v7.xlsx")
-names(all_sheets) <- sheet_names
-
-for (sheet in sheet_names) {
-  assign(sheet, read_excel(path = "categorized_tumor_errors_v7.xlsx", sheet = sheet), envir = .GlobalEnv)
-}
-
-for(sheet in sheet_names){
-  df <- get(sheet)
-  df$method_name <- sheet
-  colnames(df)[c(5,6)]<-c("method_prediction","method_eval")
-  assign(sheet, df, envir = .GlobalEnv)
-}
-
-combined_df <- do.call(rbind, lapply(sheet_names, get))
+combined_df<-read_excel(paste(data_dir,"/Error_Analysis_File.xlsx",sep=""))
 
 combined_df<- combined_df%>%filter(ground_truth!="NF")
 combined_df<-combined_df%>%filter(method_eval==0)
 
 ind<-which(combined_df$method_name=="LTE3")
-combined_df$method_name[ind]<-"LTE3+Euclidean Distance"
+combined_df$method_name[ind]<-"LTE3+Euclidean distance"
 
 ind<-which(combined_df$method_name=="ada2")
 combined_df$method_name[ind]<-"ADA-002+Euclidean distance"
@@ -64,78 +53,76 @@ ind<-which(combined_df$method_name=="lv")
 combined_df$method_name[ind]<-"Levenshtein"
 
 ind<-which(combined_df$method_name=="llama")
-combined_df$method_name[ind]<-"Llama3.0+Euclidean Distance"
+combined_df$method_name[ind]<-"Llama3.0+Euclidean distance"
 
 ind<-which(combined_df$method_name=="biobert")
-combined_df$method_name[ind]<-"BioBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"BioBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="medllama")
-combined_df$method_name[ind]<-"MedLlama_13B+Euclidean Distance"
+combined_df$method_name[ind]<-"MedLlama_13B+Euclidean distance"
 
 ind<-which(combined_df$method_name=="pubmedbert")
-combined_df$method_name[ind]<-"PubMedBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"PubMedBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="modernbert")
-combined_df$method_name[ind]<-"ModernBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"ModernBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="medllama_7b")
 combined_df$method_name[ind]<-"MedLlama2+Euclidean Distance"
 
 ind<-which(combined_df$method_name=="llama_32_3b")
-combined_df$method_name[ind]<-"Llama3.2_3B+Euclidean Distance"
+combined_df$method_name[ind]<-"Llama3.2_3B+Euclidean distance"
 
 ind<-which(combined_df$method_name=="phi4")
-combined_df$method_name[ind]<-"Phi-4+Euclidean Distance"
+combined_df$method_name[ind]<-"Phi-4+Euclidean distance"
 
 ind<-which(combined_df$method_name=="llama_33_70b")
-combined_df$method_name[ind]<-"Llama3.3_70B+Euclidean Distance"
+combined_df$method_name[ind]<-"Llama3.3_70B+Euclidean distance"
 
 ind<-which(combined_df$method_name=="MiniLM_L6_v2")
-combined_df$method_name[ind]<-"all-MiniLM-L6-v2+Euclidean Distance"
+combined_df$method_name[ind]<-"all-MiniLM-L6-v2+Euclidean distance"
 
 ind<-which(combined_df$method_name=="mpnet_base")
-combined_df$method_name[ind]<-"all-mpnet-base-v2+Euclidean Distance"
+combined_df$method_name[ind]<-"all-mpnet-base-v2+Euclidean distance"
 
 ind<-which(combined_df$method_name=="roberta")
-combined_df$method_name[ind]<-"all-roberta-large-v1+Euclidean Distance"
+combined_df$method_name[ind]<-"all-roberta-large-v1+Euclidean distance"
 
 ind<-which(combined_df$method_name=="MiniLM_L12_v2")
-combined_df$method_name[ind]<-"all-MiniLM-L12-v2+Euclidean Distance"
+combined_df$method_name[ind]<-"all-MiniLM-L12-v2+Euclidean distance"
 
 ind<-which(combined_df$method_name=="e5_large")
-combined_df$method_name[ind]<-"e5-large+Euclidean Distance"
+combined_df$method_name[ind]<-"e5-large+Euclidean distance"
 
 ind<-which(combined_df$method_name=="gtr_t5_large")
-combined_df$method_name[ind]<-"gtr-t5-large+Euclidean Distance"
+combined_df$method_name[ind]<-"gtr-t5-large+Euclidean distance"
 
 ind<-which(combined_df$method_name=="labse")
-combined_df$method_name[ind]<-"LaBSE+Euclidean Distance"
+combined_df$method_name[ind]<-"LaBSE+Euclidean distance"
 
 ind<-which(combined_df$method_name=="sciBERT")
-combined_df$method_name[ind]<-"SciBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"SciBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="sapBERT")
-combined_df$method_name[ind]<-"SapBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"SapBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="cohere")
-combined_df$method_name[ind]<-"embed-english-v2.0+Euclidean Distance"
+combined_df$method_name[ind]<-"embed-english-v2.0+Euclidean distance"
 
 ind<-which(combined_df$method_name=="deepseek")
-combined_df$method_name[ind]<-"DeepSeek_8B+Euclidean Distance"
+combined_df$method_name[ind]<-"DeepSeek_8B+Euclidean distance"
 
 ind<-which(combined_df$method_name=="BioGPT")
-combined_df$method_name[ind]<-"BioGPT+Euclidean Distance"
+combined_df$method_name[ind]<-"BioGPT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="clincalBERT")
-combined_df$method_name[ind]<-"ClinicalBERT+Euclidean Distance"
+combined_df$method_name[ind]<-"ClinicalBERT+Euclidean distance"
 
 ind<-which(combined_df$method_name=="e5large_v2")
-combined_df$method_name[ind]<-"e5-large-v2+Euclidean Distance"
+combined_df$method_name[ind]<-"e5-large-v2+Euclidean distance"
 
 ind<-which(combined_df$method_name=="nomic")
-combined_df$method_name[ind]<-"nomic+Euclidean Distance"
-
-
+combined_df$method_name[ind]<-"nomic+Euclidean distance"
 
 
 # Compute normalized error % per method
@@ -145,9 +132,6 @@ error_freq <- combined_df %>%
   group_by(method_name) %>%
   mutate(percent = 100 * count / sum(count)) %>%
   ungroup()
-#sum(error_summary$percent[error_summary$method_name=="BioGPT"])
-
-
 
 # Optionally: reorder methods by total error volume (for cleaner heatmap)
 method_order <- error_freq %>%
@@ -156,34 +140,39 @@ method_order <- error_freq %>%
   arrange(desc(total)) %>%
   pull(method_name)
 
-# Plot heatmap
-ggplot(error_freq, aes(x = error_category, y = factor(method_name, levels = method_order), fill = percent)) +
-  geom_tile(color = "grey90") +
-  scale_fill_viridis(name = "% of Errors", option = "plasma", direction = -1, begin = 0.2, end = 0.9) +
-  labs(title = "Heatmap of Error Type Frequency by Method, WHO-5th",
-       x = "Error Category",
-       y = "Method Name") +
-  theme_minimal(base_size = 12) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        panel.grid = element_blank(),
-        legend.position = "right")
 
+my_colors <- c(
+  "#1f77b4",  # blue
+  "#ff7f0e",  # orange
+  "#2ca02c",  # green
+  "#7f7f7f",  # gray
+  "#9467bd",  # purple
+  "#8c564b",  # brown
+  "#e377c2",  # pink
+  "#d62728",  # red
+  "#bcbd22",  # lime
+  "#aec7e8",   # light blue (contrast with first blue)
+  "#17becf"  # cyan
+  
+)
 
-
-
-#plot of stacked bar plot
 ggplot(error_freq, aes(x = factor(method_name, levels = method_order),
                        y = percent,
                        fill = error_category)) +
   geom_bar(stat = "identity", width = 0.8) +
-  scale_fill_brewer(palette = "Set2")+
+  scale_fill_manual(values = my_colors)+
   labs(title = "Error Category Distribution per Method, WHO-5th",
        x = "Method",
        y = "Percentage of Errors",
        fill = "Error Category") +
+  geom_bar(stat = "identity", color = "white")+
   theme_minimal(base_size = 12) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-        panel.grid.major.x = element_blank())
+  theme(axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 1),
+        panel.grid.major.x = element_blank())+
+  geom_text(aes(label = ifelse(percent > 3, paste0(round(percent), "%"), "")),
+            position = position_stack(vjust = 0.5),
+            color = "black",
+            size = 3)+ coord_flip()
 
 
 
