e7ea9e863b9f833f69c599d25ff63aaeb8e57c11
Aditya Lahiri <lahiria@chop.edu>
2025-05-27 11:44:48 -0400

    update error analysis

--- util/column_rename.R
+++ util/column_rename.R
@@ -2,12 +2,12 @@ column_rename <- function(col_names){
   
 for(iter in 1:length(col_names)){
   if(col_names[iter]=="valid_euclidean_dist_v3" ){
-    col_names[iter]="LTE-3+Euclidean Distance"
+    col_names[iter]="LTE-3+Euclidean distance"
   }else if(col_names[iter]=="valid_af_v3"){
     col_names[iter]="LTE-3+AP"
     
   }else if(col_names[iter]=="valid_euclidean_dist_ada2"){
-    col_names[iter]="ADA-002+Euclidean Distance"
+    col_names[iter]="ADA-002+Euclidean distance"
     
   }else if(col_names[iter]=="valid_af_ad2"){
     col_names[iter]="ADA-002+AP"
@@ -36,75 +36,75 @@ for(iter in 1:length(col_names)){
     col_names[iter]="Cosine+AP"
     
   }else if(col_names[iter]=="valid_euclidean_dist_llama"){
-    col_names[iter]="LLama3.0+Euclidean Distance"
+    col_names[iter]="LLama3.0+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_biobert"){
-    col_names[iter]="BioBERT+Euclidean Distance"
+    col_names[iter]="BioBERT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_medllama"){
-    col_names[iter]="MedLlama_13B+Euclidean Dist"
+    col_names[iter]="MedLlama_13B+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_pubmedbert"){
-    col_names[iter]="PubMedBERT+Euclidean Distance"
+    col_names[iter]="PubMedBERT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_modernbert"){
-    col_names[iter]="ModernBERT+Euclidean Distance"
+    col_names[iter]="ModernBERT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_medllama_7b"){
-    col_names[iter]="MedLlama2+Euclidean Distance"
+    col_names[iter]="MedLlama2+Euclidean distance"
  
   }else if(col_names[iter]=="valid_euclidean_dist_llama_32_3b"){
-    col_names[iter]="LLama3.2_3B+Euclidean Distance"
+    col_names[iter]="LLama3.2_3B+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_phi4"){
-    col_names[iter]="Phi-4+Euclidean Distance"
+    col_names[iter]="Phi-4+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_llama_33_70b"){
-    col_names[iter]="Llama3.3_70B+Euclidean Distance"
+    col_names[iter]="Llama3.3_70B+Euclidean distance"
   }else if(col_names[iter]=="valid_euclidean_dist_MiniLM_L6_v2"){
-    col_names[iter]="all-MiniLM-L6-v2+Euclidean Distance"
+    col_names[iter]="all-MiniLM-L6-v2+Euclidean distance"
   }else if(col_names[iter]=="valid_euclidean_mpnet_base"){
-    col_names[iter]="all-mpnet-base-v2+Euclidean Distance"
+    col_names[iter]="all-mpnet-base-v2+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_e5_large"){
-    col_names[iter]="e5-large+Euclidean Distance"
+    col_names[iter]="e5-large+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_gtr_t5_large"){
-    col_names[iter]="gtr-t5-large+Euclidean Distance"
+    col_names[iter]="gtr-t5-large+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_roberta"){
-    col_names[iter]="all-roberta-large-v1+Euclidean Distance"
+    col_names[iter]="all-roberta-large-v1+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_MiniLM_L12_v2"){
-    col_names[iter]="all-MiniLM-L12-v2+Euclidean Distance"
+    col_names[iter]="all-MiniLM-L12-v2+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_labse"){
-    col_names[iter]="LaBSE+Euclidean Distance"
+    col_names[iter]="LaBSE+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_sciBERT"){
-    col_names[iter]="sciBERT+Euclidean Distance"
+    col_names[iter]="sciBERT+Euclidean distance"
     
   }
   else if(col_names[iter]=="valid_euclidean_dist_sapBERT"){
-    col_names[iter]="sapBERT+Euclidean Distance"
+    col_names[iter]="sapBERT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_cohere"){
-    col_names[iter]="embed-english-v2.0+Euclidean Distance"
+    col_names[iter]="embed-english-v2.0+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_deepseek"){
-    col_names[iter]="DeepSeek_8B+Euclidean Distance"
+    col_names[iter]="DeepSeek_8B+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_BioGPT"){
-    col_names[iter]="BioGPT+Euclidean Distance"
+    col_names[iter]="BioGPT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_clincalBERT"){
-    col_names[iter]="ClinicalBERT+Euclidean Distance"
+    col_names[iter]="ClinicalBERT+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_e5large_v2"){
-    col_names[iter]="e5large_v2+Euclidean Dist"
+    col_names[iter]="e5large_v2+Euclidean distance"
     
   }else if(col_names[iter]=="valid_euclidean_dist_nomic"){
-    col_names[iter]="nomic+Euclidean Distance"
+    col_names[iter]="nomic+Euclidean distance"
   }
 }
   return(col_names)
